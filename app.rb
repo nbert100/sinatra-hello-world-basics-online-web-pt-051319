ba60e0876a58a2a606c91be5ae3eec18da55fd5c
Nia Bert <nbert14@gmail.com>
2019-08-08 22:24:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
  get '/' do 
-    "Started my server using Shotgun!"
+    "Hello, World!"
   end
 
 end
